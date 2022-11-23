554872a53232b74786df29049dbc22396b0768a8
Wil Wardrop <wwardrop@gmail.com>
2022-11-23 18:16:57 -0500

    updated fixtures

--- db.sql
+++ db.sql
@@ -1,26 +1,8 @@
-DROP TABLE hitchapi_driver_trips
 
-DROP TABLE hitchapi_location
 
+DELETE FROM hitchapi_member
+WHERE id >1
 
 DELETE FROM auth_user
-WHERE id <2
+WHERE id>1
 
-DELETE FROM hitchapi_triptag
-WHERE driver_trip_id >0
-
-DELETE FROM hitchapi_drivertrip
-WHERE id >2
-
-DELETE FROM hitchapi_driverpassengertrip
-WHERE id >0
-
-
-DELETE FROM hitchapi_passengertrip
-WHERE id >0
-
-DELETE FROM authtoken_token
-WHERE user_id <2
-
-DELETE FROM hitchapi_message
-WHERE id >0
